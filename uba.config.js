7aabb1a0245e38675f1f43ca2a36721276beae7a
gct <gct@yonyou.com>
2017-11-13 16:41:23 +0800

    t

--- uba.config.js
+++ uba.config.js
@@ -21,7 +21,7 @@ const svrConfig = {
   port: 3000
 };
 const proxyConfig = [{
-  enable : true,//如果为true那么就是启用代理，mock会失效，为false的时候代理失效，mock生效
+  enable : false,//如果为true那么就是启用代理，mock会失效，为false的时候代理失效，mock生效
   router: "/",
 
   //url: "http://10.11.65.50:8080",
